b262b7a3653b0290f13e4445a2a22d2c4b2e2120
solimanfouad <solimanfouad5@gmail.com>
2026-09-11 18:07:15 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-const C="media-pwa-v3";
+const C="media-pwa-v4";
 self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest","./icon.svg"]))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request))));
