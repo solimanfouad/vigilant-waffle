d0af1cf12999817248cbf585cc81d902d0b2c6a7
solimanfouad <solimanfouad5@gmail.com>
2026-09-11 17:29:17 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-const C="media-pwa-v2";
-self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest","./icon.svg"]))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request))));
