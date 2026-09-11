84eebd4f8abf152ae14c3dd5203471b849677e6f
solimanfouad <solimanfouad5@gmail.com>
2026-09-11 18:09:47 +0300

    Upgrade service worker to version 5 with activation

--- sw.js
+++ sw.js
@@ -1,3 +1,10 @@
-const C="media-pwa-v4";
-self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest","./icon.svg"]))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request))));
+const C="media-pwa-v5";
+self.addEventListener("install", e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(C).then(c => c.addAll(["./","./index.html","./manifest.webmanifest","./icon.svg"])));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== C && caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener("fetch", e => e.respondWith(caches.match(e.request).then(x => x || fetch(e.request))));
